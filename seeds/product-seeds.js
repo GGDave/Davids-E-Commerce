3cec52cdf99f9b941065d233ef0116f5c8f7e4c0
David Flores <dcflores@live.com>
2023-05-21 16:27:42 -0700

    updated seeds file

--- seeds/product-seeds.js
+++ seeds/product-seeds.js
@@ -2,31 +2,36 @@ const { Product } = require('../models');
 
 const productData = [
   {
-    product_name: 'Plain T-Shirt',
+    name: 'Plain T-Shirt', //modified name parameter
+    description: 'A plain t-shirt',  // Added 'description'
     price: 14.99,
     stock: 14,
     category_id: 1,
   },
   {
-    product_name: 'Running Sneakers',
+    name: 'Running Sneakers',//modified name parameter
+    description: 'High quality running sneakers',  // Added 'description'
     price: 90.0,
     stock: 25,
     category_id: 5,
   },
   {
-    product_name: 'Branded Baseball Hat',
+    name: 'Branded Baseball Hat',//modified name parameter
+    description: 'A branded baseball hat',  // Added 'description'
     price: 22.99,
     stock: 12,
     category_id: 4,
   },
   {
-    product_name: 'Top 40 Music Compilation Vinyl Record',
+    name: 'Top 40 Music Compilation Vinyl Record',//modified name parameter
+    description: 'Top 40 Music Compilation on Vinyl',  // Added 'description'
     price: 12.99,
     stock: 50,
     category_id: 3,
   },
   {
-    product_name: 'Cargo Shorts',
+    name: 'Cargo Shorts',//modified name parameter
+    description: 'Comfortable cargo shorts',  // Added 'description'
     price: 29.99,
     stock: 22,
     category_id: 2,
